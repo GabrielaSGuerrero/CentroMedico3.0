27a0732f723182c2072eddeb163a8cd9d36e7b57
Gaby <Gaby@DESKTOP-NFDNEUN>
2020-11-20 18:16:22 -0300

    comienzo de implementacion Internacion

--- CentroMedico/src/Internacion.java
+++ CentroMedico/src/Internacion.java
@@ -1,4 +1,16 @@
 
 public class Internacion {
+	double valor;
+	boolean deuda;
+	//int matricula;
+	Fecha fecha;
+	Fecha fechaAlta;
+	double costoTotal;
+	String area;
 
+	Internacion(Fecha ingreso, String area){
+		this.fecha = ingreso;
+		this.area = area;
+	}
+	
 }
