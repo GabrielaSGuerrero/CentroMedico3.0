fd7746eb6ef972acbd344726af481b3a87b413d6
matias <matias@matiasjacob>
2020-11-22 21:07:56 -0300

    TAD Medico (constructor y operaciones) y metodos ToString implementado

--- CentroMedico/src/Medico.java
+++ CentroMedico/src/Medico.java
@@ -1,4 +1,36 @@
 
 public class Medico {
+	String nombre;
+	int matricula;
+	double honorarios;
+	Especialidad especialidad;
+
+	Medico (String n, int mat, double hono, Especialidad especialidad){ //Constructor
+		this.nombre = n;
+		this.matricula= mat;
+		this.honorarios= hono;
+		this.especialidad= especialidad;
+	}
+
+	
+	public double getHonorarios() {
+		return honorarios;
+	}
+
+	public Especialidad getEspecialidad() {
+		return especialidad;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Medico [nombre=" + nombre + ", matricula=" + matricula + ", honorarios=" + honorarios
+				+ ", especialidad=" + especialidad + "]";
+	}
+	
+	
 
 }
+
+
+		
