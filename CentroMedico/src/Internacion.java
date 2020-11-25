f22916a6060d32cea8887333962ce0cc8f9f689d
Gaby <Gaby@DESKTOP-NFDNEUN>
2020-11-25 01:04:55 -0300

    Equals de Internacion, con criterio de Fecha

--- CentroMedico/src/Internacion.java
+++ CentroMedico/src/Internacion.java
@@ -26,4 +26,28 @@ public class Internacion {
 	public void setFechaAlta(Fecha f) {
 		this.fechaAlta.setCalendario(f.getDia(), f.getMes(), f.getAnio());
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fecha == null) ? 0 : fecha.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Internacion other = (Internacion) obj;
+		if (fecha == null) {
+			if (other.fecha != null)
+				return false;
+		} else if (!fecha.equals(other.fecha))
+			return false;
+		return true;
+	}
+	
 }
